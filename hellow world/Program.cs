5e3cfc84261d69c737fc441e3da61a95941db10d
Armageddontoday <armagedoontoday@mail.ru>
2016-09-20 00:41:08 +0300

    Hellow!
    add project files

---
+++ hellow world/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hellow_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow world!");
+            Console.ReadKey(); //ожидание нажатия клавиши, дабы окно не закрывалось сразу
+        }
+    }
+}
