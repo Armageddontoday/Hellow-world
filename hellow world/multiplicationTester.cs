8876cb5c7d54feff078f622559e7a249ce55b2ad
Ivan <armageddontoday@mail.ru>
2016-10-05 17:32:03 +0300

    немного изменений
    изменен ход проверки ответа пользователя

--- hellow world/multiplicationTester.cs
+++ hellow world/multiplicationTester.cs
@@ -6,52 +6,37 @@ using System.Threading.Tasks;
 
 namespace HellowWorld
 {
+    
     class MultiplicationTester
     {
-        /// <summary>
-        ///  попытка преобразовать введённую строку в целое число и, в случае успеха, проверить верен ли ответ
-        ///  в случае неправильного ответа программа не будет менять исходные значения num1 и num2, предлагая ввести их верное произведение
-        ///  но можно изменить их командой "next"
-        /// </summary>
-        /// <param name="input">введённая пользователем строка</param>
-        private void AnswerCheck(int num1, int num2)
+        private int AnswerCheck(int num1,int num2)
         {
+            
             var uiHelper = new UIHelper();
+            string userInput = uiHelper.GetUserAnswer();
             int numResult = 0;
-            while (true)                
+            if (userInput == "exit" || userInput == "Exit") //проверка на ввод команды "выход"
+                Environment.Exit(0);
+            if (userInput == "next" || userInput == "Next")
             {
-                
-                string userInput = uiHelper.GetUserAnswer();
-                  if (userInput == "exit" || userInput == "Exit") //проверка на ввод команды "выход"
-                    Environment.Exit(0);
-                if (userInput == "next" || userInput == "Next")
-                {
-                    Console.Clear();
-                    uiHelper.ShowGreetingMessage();
-                    break;
-                }
-              try 
-                {numResult = int.Parse(userInput);}
-              catch //при провале преобразования(введено некорректное число\ничего не введено\символы) сообщение об ошибке
-                {
-                 uiHelper.ShowErrorMessage(); //в случае неправильного ответа покаывается сообщение и запрашивается повторный ввод при тех же исходных num1 и num2
-                 Console.Write("{0} * {1}= ", num1, num2);
-                 continue;
-                }
-
-                if (numResult == num1 * num2)//правильный ответ?
-                {
-                    uiHelper.ShowCorrectAnswer(num1, num2, numResult);
-                    break;//при правильном ответе- сообщение и новые num1, num2
-                }
-                else
-                {
-                    uiHelper.ShowWrongAnswer(num1, num2, numResult);
-                    Console.Write("{0} * {1}= ", num1, num2);//в случае неправильного ответа покаывается сообщение и запрашивается повторный ввод при тех же исходных num1 и num2
-                }
+                Console.Clear();
+                StartTest();
+             }
+            try
+            {
+                numResult = int.Parse(userInput);
+                return numResult;
             }
+            catch //при провале преобразования(введено некорректное число\ничего не введено\символы) сообщение об ошибке
+            {
+                uiHelper.ShowErrorMessage(); //в случае неправильного ответа покаывается сообщение и запрашивается повторный ввод при тех же исходных num1 и num2
+                Console.Write("{0} * {1}= ", num1, num2);
+                numResult= AnswerCheck(num1, num2);
+            }
+            return numResult;
 
-        }
+        } 
+       
         /// <summary>
         ///   показывает приветственное сообщеие и затем уходит в бесконечный цикл выполнения программы
         /// </summary>
@@ -59,14 +44,27 @@ namespace HellowWorld
         {
             var uiHelper = new UIHelper();
             var randNum = new Random();
+            int numResult = 0;
             uiHelper.ShowGreetingMessage();
             while (true) //бесконечный цикл, программа завершится только в случае команды "выход"        
             {//при каждом прохождении цикла меняются исходные значения
                 int num1 = randNum.Next(1, 10);
                 int num2 = randNum.Next(1, 10);
+                
                 Console.Write("{0} * {1}= ", num1, num2);
-                AnswerCheck(num1, num2);
-            }
+                do
+                {
+                    numResult = AnswerCheck(num1, num2);
+                    if (numResult != num1 * num2)
+                    {
+                        uiHelper.ShowWrongAnswer(num1, num2, numResult);
+                        Console.Write("{0} * {1}= ", num1, num2);
+                    }
+                } while (numResult != num1 * num2);
+                Console.Clear();
+                uiHelper.ShowCorrectAnswer(num1, num2, numResult);
+
+          }
         }
     }
 }
