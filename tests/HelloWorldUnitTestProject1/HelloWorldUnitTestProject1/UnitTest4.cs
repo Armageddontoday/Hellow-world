a15c0ce732cdea6fd0055ae39724498dc6a36b5e
Ivan <armageddontoday@mail.ru>
2016-12-14 23:30:43 +0300

    добавление тестов
    добавлены тесты методов классов- мат. операций и класса Task

---
+++ tests/HelloWorldUnitTestProject1/HelloWorldUnitTestProject1/UnitTest4.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System;
+using HellowWorld;
+using Moq;
+using NUnit;
+
+//namespace HelloWorld
+namespace NUnit.Framework.Tests
+{
+    /// <summary>
+    /// тесты правильности возвращаемого символа
+    /// </summary>
+    [TestFixture]
+    public class TaskTest
+    {
+        [Test]
+        public void TaskInitialization()
+        {
+            var iMathOperationMock = new Mock<IMathOperation>();
+            iMathOperationMock.Setup(a => a.GetResult(2,3)).Returns(5);           
+            var task = new Task(2,3, iMathOperationMock.Object, null);
+            Assert.AreEqual(task.Operand1, 2);
+            Assert.AreEqual(task.Operand2, 3);
+            Assert.AreEqual(task.CorrectAnswer, 5);
+        }        
+    }
+}
